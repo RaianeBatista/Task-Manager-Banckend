c4b5617cfb826726692dae48715647c3a497d55b
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2024-11-20 16:42:46 -0300

    feat: add cors

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const TaskRouter = require("./src/routes/task.routes");
+const cors = require('cors');
 
+const TaskRouter = require("./src/routes/task.routes");
 const connectToDataBase = require("./src/database/mongoose.databse");
 
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 connectToDataBase();
